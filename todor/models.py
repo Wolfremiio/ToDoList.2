d0960a5fd584479ccd0454f8ffe85520f1237edc
Wolfremiio <101276349+Wolfremiio@users.noreply.github.com>
2024-07-17 20:10:09 +0200

    Finish User model

--- todor/models.py
+++ todor/models.py
@@ -5,3 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+
+
+def _init_(self, userName, password):
+    self.userName = userName
+    self.password = password
+
+
+def _repr_(self):
+    return f"<User: {self.username}> "
