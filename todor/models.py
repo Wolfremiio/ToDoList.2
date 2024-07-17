56d902849ec847d583f105acfe4e6d4ba85d0722
Wolfremiio <101276349+Wolfremiio@users.noreply.github.com>
2024-07-17 20:44:21 +0200

    Finish Todo model

--- todor/models.py
+++ todor/models.py
@@ -14,3 +14,22 @@ def _init_(self, userName, password):
 
 def _repr_(self):
     return f"<User: {self.username}> "
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_by = db.Column(db.Integer, db.models.ForeignKey("user.id"), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.Text)
+    state = db.Column(db.Boolean, default=False)
+
+
+def _init_(self, created_by, title, desc, state=False):
+    self.created_by = created_by
+    self.title = title
+    self.desc = desc
+    self.state = state
+
+
+def _repr_(self):
+    return f"<Todo: {self.title}> "
